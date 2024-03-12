9d0eec78ace4bb8dab47478d8545c09e294f1e12
Nikolai Golub <nikolay.v.golub@gmail.com>
2024-03-12 15:47:18 +0100

    AccountInfo hash nonce check

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 
 
 run:
-	RUST_LOG="[executor]=info" RISC0_DEV_MODE=1 cargo run
+	RUST_LOG="[executor]=info,host=info" RISC0_DEV_MODE=1 cargo run
--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -3,19 +3,54 @@
 #![no_std]  // std support is experimental
 
 
+use revm_primitives::AccountInfo;
+
 use risc0_zkvm::guest::env;
 
 risc0_zkvm::guest::entry!(main);
 
 
+use core::hash::Hash;
+use sha2::{Digest, Sha256};
+
+struct SimpleSha256Hasher {
+    hasher: Sha256,
+}
+
+impl SimpleSha256Hasher {
+    fn new() -> Self {
+        SimpleSha256Hasher { hasher: Sha256::new() }
+    }
+
+    fn result(self) -> [u8; 32]  {
+        self.hasher.finalize().into()
+    }
+}
+
+impl core::hash::Hasher for SimpleSha256Hasher {
+    fn finish(&self) -> u64 {
+        0
+    }
+
+    fn write(&mut self, bytes: &[u8]) {
+        self.hasher.update(bytes);
+    }
+}
+
 fn main() {
-    // TODO: Implement your guest code here
+    let nonce: u64 = env::read();
+    let account_info: AccountInfo = env::read();
+    let input_result: [u8; 32] = env::read();
 
-    // read the input
-    let input: u32 = env::read();
+    let mut hasher = SimpleSha256Hasher::new();
+    account_info.hash(&mut hasher);
 
-    // TODO: do something with the input
+    let result = hasher.result();
+
+    assert_eq!(result, input_result, "hashes do not match");
+    assert_eq!(nonce, account_info.nonce, "nonce do not match");
+
 
     // write public output to the journal
-    env::commit(&input);
+    env::commit(&nonce);
 }
